cf58b95e46436a57b73e53ad4304bd3bbf0395d7
Ваше имя < >
2021-03-12 12:42:27 +0300

    текст комментария

---
+++ AutoService/AutoService/windows/InputBoxWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AutoService.windows;
+
+
+namespace AutoService.windows
+{
+    /// <summary>
+    /// Логика взаимодействия для InputBoxWindow.xaml
+    /// </summary>
+    public partial class InputBoxWindow : Window
+    {
+        public InputBoxWindow(string Caption)
+        {
+            InitializeComponent();
+            WindowCaption = Caption;
+            this.DataContext = this;
+        }
+        public string WindowCaption { get; set; }
+        public string InputText { get; set; }
+
+
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
